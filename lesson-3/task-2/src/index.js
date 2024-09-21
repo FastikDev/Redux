cae47315434f3185b21f353c87c5f1226532707e
FastikDev <sir.fastik18@gmail.com>
2024-09-21 18:24:02 +0300

    Redux dev tool

---
+++ lesson-3/task-2/src/index.js
@@ -0,0 +1,8 @@
+import store, { decrement, increment } from './counter';
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch(increment());
